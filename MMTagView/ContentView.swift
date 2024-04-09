ead594bb1e81701ee5ce1100f40fab3fcaf662f5
MksMil <maksymmiliaiev@gmail.com>
2024-04-09 19:57:38 +0300

    Fixed 'general view' height changing for 'last subview' positioning.

--- MMTagView/ContentView.swift
+++ MMTagView/ContentView.swift
@@ -14,21 +14,36 @@ struct ContentView: View {
 //        "Color.red","Color.green","Color.blue","asdsfsdjgak",
 //                           "Color.red","Color.green2","Color.blue3","a4sdsfsdjgak",
 //                           "Color.red","Color.green7","Color.blue6","asds5fsdjgak"
-        Color.red, Color.green, Color.blue
+        Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red,
                           ]
     
     var body: some View {
-        VStack {
-            GenericTagView(sourceContent: source,
-                           returnedTags: $tags) {
-                RoundedRectangle(cornerRadius: 25.0).fill(.ultraThinMaterial)
-            } cellView: { col in
-                Circle().fill(col)
-//                Text(col)
-//                    .fixedSize().padding(.horizontal,4)
-//                    .background(RoundedRectangle(cornerRadius: 5).fill( Color.blue))
-                    .frame(width: 40,height: 40)
-            }
+        ScrollView {
+            TagLayout(hSpacing: 5,vSpacing: 5){
+                
+                    ForEach(source.indices, id: \.self) { index in
+                        Circle().fill(source[index]).frame(width: 50)
+                        Text("\(index)")
+                    }.frame(maxWidth: .infinity)
+                    Text("asdf")
+                
+            }.padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 10).fill(.ultraThinMaterial)
+                }
+            
+            
+//            GenericTagView(sourceContent: source,
+//                           returnedTags: $tags) {
+//                RoundedRectangle(cornerRadius: 25.0).fill(.ultraThinMaterial)
+//            } cellView: { col in
+//                Circle().fill(col)
+////                Text(col)
+////                    .fixedSize().padding(.horizontal,4)
+////                    .background(RoundedRectangle(cornerRadius: 5).fill( Color.blue))
+//                    .frame(width: 40,height: 40)
+//            }
+////            .padding()
 
         }
     }
--- MMTagView/TagViewLayout.swift
+++ MMTagView/TagViewLayout.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TagViewLayout: View {
     var body: some View {
         ScrollView{
-            TagLayout(hSpacing: 8, vSpacing: 5){
+            TagLayout(hSpacing: 10,vSpacing: 10){
                 Circle().fill(.red).frame(width: 100, height: 100)
                 Rectangle().fill(.blue).frame(width: 100, height: 100)
                 RoundedRectangle(cornerRadius: 25.0).stroke(Color.orange, lineWidth: 1).frame(width: 100, height: 100)
@@ -35,32 +35,34 @@ struct TagViewLayout: View {
                 Image(systemName: "sun.horizon")
                     .resizable()
                     .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-//                Image(systemName: "sun.horizon")
-//                    .resizable()
-//                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Text("Hellp")
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
+                Image(systemName: "sun.horizon")
+                    .resizable()
+                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
                 
             }
             .border(Color.black,width: 3)
+            .padding(10)
             .frame(width: 340)
-            .padding()
+            //            .padding()
             .border(.red, width: 3)
             .background(.gray)
         }
@@ -69,6 +71,7 @@ struct TagViewLayout: View {
 
 #Preview {
     TagViewLayout()
+//    ContentView()
 }
 
 // MARK: - Layout
@@ -80,38 +83,54 @@ struct TagLayout: Layout{
     
     func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
         let totalWidth = proposal.width ?? proposal.replacingUnspecifiedDimensions().width
-        
         var width = Double.zero
         var height = subviews.isEmpty ? 0: subviews[0].sizeThatFits(.unspecified).height
         var maxAddedHeight = Double.zero
+        var firstElementHeight: Double = .zero
         
         for index in subviews.indices{
             let size = subviews[index].sizeThatFits(.unspecified)
             if (width + size.width) > totalWidth {
-                width = size.width
-                height += maxAddedHeight + vSpacing
-                maxAddedHeight = size.height
+                //next row
+                if index == subviews.count - 1 {
+                    // if last
+                    height += maxAddedHeight + size.height
+                } else {
+                    width = size.width
+                    maxAddedHeight = size.height
+                    height += maxAddedHeight + vSpacing
+                    firstElementHeight = size.height
+                }
+            
             } else {
-                width += size.width + hSpacing
-                maxAddedHeight = max(maxAddedHeight, size.height)
-            }
+                //current row
+                if index == subviews.count - 1 {
+                    //if last
+                    height -= firstElementHeight
+                    maxAddedHeight = max(maxAddedHeight, size.height)
+                    height += maxAddedHeight
+                } else {
+                    width += size.width + hSpacing
+                    maxAddedHeight = max(maxAddedHeight, size.height)
+                }
+                }
         }
-        
         return CGSize(width: totalWidth, height: height)
     }
     
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
         var point = bounds.origin
-        let maxX = bounds.width
+        let maxX = bounds.width + bounds.origin.x
         var maxAddedHeight = Double.zero
         for subview in subviews {
             let size = subview.sizeThatFits(.unspecified)
-            if (point.x + size.width) < maxX {
-//                let pointToplace = CGPoint(x: point.x, y: point.y + maxAddedHeight / 2 - (maxAddedHeight > 0 ? size.height / 2 : 0))
+            if (point.x + size.width) <= maxX {
+                //current row
                 subview.place(at: point, anchor: .topLeading, proposal: .unspecified)
                 point.x += size.width + hSpacing
                 maxAddedHeight = max(maxAddedHeight, size.height)
             } else {
+                //next row
                 point.x = bounds.origin.x
                 point.y += maxAddedHeight + vSpacing
                 maxAddedHeight = size.height
