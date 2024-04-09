3a6d666c0511dd58bd887b8372a7697645831ffd
MksMil <maksymmiliaiev@gmail.com>
2024-04-09 20:14:05 +0300

    fixed bug with empty subviews in layout, and bug with one row height for layout size

--- MMTagView/ContentView.swift
+++ MMTagView/ContentView.swift
@@ -10,41 +10,37 @@ import SwiftUI
 struct ContentView: View {
     
     @State var tags: [Color] = []
-    var source: [Color] = [
-//        "Color.red","Color.green","Color.blue","asdsfsdjgak",
-//                           "Color.red","Color.green2","Color.blue3","a4sdsfsdjgak",
-//                           "Color.red","Color.green7","Color.blue6","asds5fsdjgak"
-        Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red, Color.green, Color.blue,Color.red,
-                          ]
+    var source: [Color] = [Color.red, Color.green, Color.blue,
+                           Color.red, Color.green, Color.blue,
+                           Color.red, Color.green, Color.blue,
+                           Color.red, Color.green, Color.blue,
+                           Color.red, Color.green, Color.blue,Color.orange]
     
     var body: some View {
         ScrollView {
             TagLayout(hSpacing: 5,vSpacing: 5){
                 
-                    ForEach(source.indices, id: \.self) { index in
-                        Circle().fill(source[index]).frame(width: 50)
-                        Text("\(index)")
-                    }.frame(maxWidth: .infinity)
-                    Text("asdf")
-                
+                ForEach(tags.indices, id: \.self) { index in
+                    Circle().fill(tags[index]).frame(width: 50)
+                }.frame(maxWidth: .infinity)
             }.padding()
                 .background {
                     RoundedRectangle(cornerRadius: 10).fill(.ultraThinMaterial)
                 }
             
             
-//            GenericTagView(sourceContent: source,
-//                           returnedTags: $tags) {
-//                RoundedRectangle(cornerRadius: 25.0).fill(.ultraThinMaterial)
-//            } cellView: { col in
-//                Circle().fill(col)
-////                Text(col)
-////                    .fixedSize().padding(.horizontal,4)
-////                    .background(RoundedRectangle(cornerRadius: 5).fill( Color.blue))
-//                    .frame(width: 40,height: 40)
-//            }
-////            .padding()
-
+                        GenericTagView(sourceContent: source,
+                                       returnedTags: $tags) {
+                            RoundedRectangle(cornerRadius: 25.0).fill(.ultraThinMaterial)
+                        } cellView: { col in
+                            Circle().fill(col)
+            //                Text(col)
+            //                    .fixedSize().padding(.horizontal,4)
+            //                    .background(RoundedRectangle(cornerRadius: 5).fill( Color.blue))
+                                .frame(width: 40,height: 40)
+                        }
+            //            .padding()
+            
         }
     }
 }
--- MMTagView/TagViewLayout.swift
+++ MMTagView/TagViewLayout.swift
@@ -14,55 +14,11 @@ struct TagViewLayout: View {
                 Circle().fill(.red).frame(width: 100, height: 100)
                 Rectangle().fill(.blue).frame(width: 100, height: 100)
                 RoundedRectangle(cornerRadius: 25.0).stroke(Color.orange, lineWidth: 1).frame(width: 100, height: 100)
-                Circle().fill(.red).frame(width: 100, height: 100)
-                Rectangle().fill(.blue).frame(width: 100, height: 100)
-                Text("Hello World").fixedSize()
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Circle().fill(.orange).frame(width: 200,height: 200)
-                Rectangle().fill(.blue).frame(width: 100, height: 100)
-                RoundedRectangle(cornerRadius: 25.0).stroke(Color.orange, lineWidth: 1).frame(width: 100, height: 100)
-                Circle().fill(.red).frame(width: 100, height: 100)
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Text("Goodbay")
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Text("Hellp")
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                Image(systemName: "sun.horizon")
-                    .resizable()
-                    .frame(idealWidth: 150, idealHeight: 150)//.scaledToFit()
-                
             }
             .border(Color.black,width: 3)
             .padding(10)
             .frame(width: 340)
-            //            .padding()
+            .padding()
             .border(.red, width: 3)
             .background(.gray)
         }
@@ -71,22 +27,24 @@ struct TagViewLayout: View {
 
 #Preview {
     TagViewLayout()
-//    ContentView()
+    //    ContentView()
 }
 
 // MARK: - Layout
 
+// MARK: - Any View and Size Grid Layout
 struct TagLayout: Layout{
     
     var hSpacing: Double
     var vSpacing: Double
     
     func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        guard !subviews.isEmpty else { return proposal.replacingUnspecifiedDimensions() }
         let totalWidth = proposal.width ?? proposal.replacingUnspecifiedDimensions().width
         var width = Double.zero
         var height = subviews.isEmpty ? 0: subviews[0].sizeThatFits(.unspecified).height
         var maxAddedHeight = Double.zero
-        var firstElementHeight: Double = .zero
+        var firstElementHeight: Double = subviews[0].sizeThatFits(.unspecified).height
         
         for index in subviews.indices{
             let size = subviews[index].sizeThatFits(.unspecified)
@@ -101,7 +59,7 @@ struct TagLayout: Layout{
                     height += maxAddedHeight + vSpacing
                     firstElementHeight = size.height
                 }
-            
+                
             } else {
                 //current row
                 if index == subviews.count - 1 {
@@ -113,7 +71,7 @@ struct TagLayout: Layout{
                     width += size.width + hSpacing
                     maxAddedHeight = max(maxAddedHeight, size.height)
                 }
-                }
+            }
         }
         return CGSize(width: totalWidth, height: height)
     }
